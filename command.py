89e229364a75e3e26694125c54403a7ea517d326
edo1z <endo.yuta@logicky.com>
2024-12-21 16:26:47 +0900

    command修正

--- command.py
+++ command.py
@@ -9,6 +9,7 @@ class State(TypedDict):
 
 
 def a(state: State):
+    print("a start!")
     number = interrupt("please input number: ")
     return Command(goto="b", update={"num": number})
 
@@ -21,15 +22,11 @@ builder = StateGraph(State)
 builder.add_node(a)
 builder.add_node(b)
 builder.set_entry_point("a")
-
 graph = builder.compile(checkpointer=MemorySaver())
-graph.get_graph().draw_mermaid_png(output_file_path="workflow.png")
 
-
-if __name__ == "__main__":
-    config = {"configurable": {"thread_id": "1"}}
-    for event in graph.stream({"num": 0}, config=config):
-        if "__interrupt__" in event:
-            user_input = input(event["__interrupt__"][0].value)
-            result = graph.invoke(Command(resume=int(user_input)), config=config)
-            print("result:", result["num"])
+config = {"configurable": {"thread_id": "1"}}
+for event in graph.stream({"num": 1}, config=config):
+    if "__interrupt__" in event:
+        user_input = input(event["__interrupt__"][0].value)
+        result = graph.invoke(Command(resume=int(user_input)), config=config)
+        print("result:", result["num"])
