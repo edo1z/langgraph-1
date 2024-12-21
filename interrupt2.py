d7904a538503c44e46e6fb44f8d53d7b042b63bf
edo1z <endo.yuta@logicky.com>
2024-12-21 14:59:01 +0900

    commnad結果だけ表示

--- interrupt2.py
+++ interrupt2.py
@@ -27,10 +27,15 @@ graph.get_graph().draw_mermaid_png(output_file_path="workflow.png")
 
 
 if __name__ == "__main__":
-    config = {"configurable": {"thread_id": "1"}}
-    result = None
-    for event in graph.stream({"num": 0}, config=config):
+    config = {"configurable": {"thread_id": "test_1"}}
+    user_input = input("input number: ")
+    for event in graph.stream({"num": int(user_input)}, config=config):
+        print("event", event)
         if "__interrupt__" in event:
             user_input = input(event["__interrupt__"][0].value)
-            result = graph.invoke(Command(resume=int(user_input)), config=config)
-    print(result)
+            for response in graph.stream(
+                Command(resume=int(user_input)),
+                stream_mode="updates",
+                config=config,
+            ):
+                print("response", response)
